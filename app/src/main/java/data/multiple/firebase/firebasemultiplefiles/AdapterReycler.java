0022b8bc617df3245cfdced41d17b24fa2dd872b
Mauro Caredda <mauro.dev88@gmail.com>
2018-01-05 12:49:50 +0100

    fix projector

--- app/src/main/java/data/multiple/firebase/firebasemultiplefiles/AdapterReycler.java
+++ app/src/main/java/data/multiple/firebase/firebasemultiplefiles/AdapterReycler.java
@@ -1,10 +1,12 @@
 package data.multiple.firebase.firebasemultiplefiles;
 
 import android.content.Context;
+import android.os.Build;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.List;
 
@@ -16,10 +18,12 @@ public class AdapterReycler extends RecyclerView.Adapter<AdapterReycler.ViewHold
 
   Context mContext;
   private List<ImageModel>mListModel;
+  private List<String>mNameList;
 
-  public AdapterReycler(Context mContext, List<ImageModel> mListModel) {
+  public AdapterReycler(Context mContext, List<ImageModel> mListModel, List<String> mNameList) {
     this.mContext = mContext;
     this.mListModel = mListModel;
+    this.mNameList = mNameList;
   }
 
   @Override public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -34,6 +38,18 @@ public class AdapterReycler extends RecyclerView.Adapter<AdapterReycler.ViewHold
     ImageModel mImageData = mListModel.get(position);
     holder.title.setText(mImageData.getmName());
 
+    String donePos = mNameList.get(position);
+    if (donePos.equals("uploading")){
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        holder.done.setImageDrawable(mContext.getDrawable(R.drawable.ic_star_upload));
+      }
+    }
+    else if(donePos.equals("completed")){
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        holder.done.setImageDrawable(mContext.getDrawable(R.drawable.ic_completed_upload));
+      }
+    }
+
   }
 
   @Override public int getItemCount() {
@@ -42,9 +58,11 @@ public class AdapterReycler extends RecyclerView.Adapter<AdapterReycler.ViewHold
 
   public class ViewHolder extends RecyclerView.ViewHolder {
     TextView title;
+    ImageView done;
     public ViewHolder(View itemView) {
       super(itemView);
       title = itemView.findViewById(R.id.filename_txt);
+      done = itemView.findViewById(R.id.image_done);
     }
   }
 }
--- app/src/main/java/data/multiple/firebase/firebasemultiplefiles/MainActivity.java
+++ app/src/main/java/data/multiple/firebase/firebasemultiplefiles/MainActivity.java
@@ -7,40 +7,78 @@ import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
 import android.provider.OpenableColumns;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity  {
 
   private RecyclerView mRecycler;
   private RecyclerView.LayoutManager mLayoutManager;
   private AdapterReycler mAdapterRecyler;
-  private Button mBtnUpload;
   private static final int RESULT_IMAGES = 1023;
   private List<ImageModel>mList;
+  private List<String>mListStatus;
   private LinearLayout mEmptyView;
+  private StorageReference mFireBaseRef;
+  private ProgressBar mProgressBar;
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
+
+  private BottomNavigationView mBottomNav ;
+  private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+      switch (item.getItemId()){
+        case R.id.upload_menu:
+          Intent openWindow = new Intent(Intent.ACTION_GET_CONTENT);
+          openWindow.setType("image/*");
+          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            openWindow.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true);
+          }
+          startActivityForResult(Intent.createChooser(openWindow,"Select Images"),RESULT_IMAGES);
+          Toast.makeText(MainActivity.this, "Upload", Toast.LENGTH_SHORT).show();
+          return true;
+
+        case R.id.login:
+          Toast.makeText(MainActivity.this, "Login", Toast.LENGTH_SHORT).show();
+          return true;
+      }
+      return false;
+    }
+  };
+  protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    mBottomNav = findViewById(R.id.bottom_nav);
+    mProgressBar = findViewById(R.id.progress_done);
+    mBottomNav.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
     mEmptyView = findViewById(R.id.emptyView);
     mList = new ArrayList<>();
-    mBtnUpload = findViewById(R.id.btn_upload);
-    mBtnUpload.setOnClickListener(this);
+    mListStatus = new ArrayList<>();
     mRecycler = findViewById(R.id.recyclerview);
     mLayoutManager = new LinearLayoutManager(this);
     mRecycler.setLayoutManager(mLayoutManager);
     mRecycler.setHasFixedSize(true);
-    mAdapterRecyler = new AdapterReycler(MainActivity.this,mList);
+    mAdapterRecyler = new AdapterReycler(MainActivity.this,mList,mListStatus);
     mRecycler.setAdapter(mAdapterRecyler);
 
   }
@@ -66,37 +104,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
   }
 
-  @Override public void onClick(View v) {
-    if (v == mBtnUpload){
-      Intent openWindow = new Intent(Intent.ACTION_GET_CONTENT);
-      openWindow.setType("image/*");
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-        openWindow.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true);
-      }
-      startActivityForResult(Intent.createChooser(openWindow,"Select Images"),RESULT_IMAGES);
-    }
 
-  }
-
-  @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+  @Override protected void onActivityResult(int requestCode, int resultCode, final Intent data) {
     if (requestCode == RESULT_IMAGES && resultCode == RESULT_OK){
       if (data.getClipData() != null){
 
         int allData = data.getClipData().getItemCount();
 
         for (int i = 0; i < allData; i++){
-
           Uri uri = data.getClipData().getItemAt(i).getUri();
-
           String nameFile = getRightPathFromProvider(MainActivity.this,uri);
           mList.add(new ImageModel(nameFile));
-          Log.d("DATA"," " + nameFile);
-          mAdapterRecyler.notifyDataSetChanged();
+          mListStatus.add("uploading");
+
+          //FireBase Storage
+          mFireBaseRef = FirebaseStorage.getInstance().getReference().child("/images").child(nameFile);
+          final int finalI = i;
+          mFireBaseRef.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+              mAdapterRecyler.notifyDataSetChanged();
+              mListStatus.remove(finalI);
+              mListStatus.add(finalI,"completed");
+              Toast.makeText(MainActivity.this, "Completed", Toast.LENGTH_SHORT).show();
+            }
+          })
+              .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+
+                  double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                  mProgressBar.setProgress((int) progress);
+
+                }
+              });
+
         }
 
         Toast.makeText(this, "Images Selected", Toast.LENGTH_SHORT).show();
-
-
 
 
       }
